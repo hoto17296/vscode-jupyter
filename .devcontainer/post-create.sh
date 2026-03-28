be752dbf5093e1a6b2a87ce21ee302e84158b3ab
Yuki Ishikawa <hoto@y32.jp>
2026-03-28 13:53:30 +0000

    modernize

--- .devcontainer/post-create.sh
+++
@@ -1,8 +0,0 @@
-#/bin/bash -eu
-
-WORKSPACE_DIR=$(cd $(dirname $0)/..; pwd)
-cd ${WORKSPACE_DIR}
-
-git config --global --add safe.directory ${WORKSPACE_DIR}
-
-pip install -r /workspace/requirements.txt
